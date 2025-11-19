6fcf8b51251cabfd0eef6a093909e4cc45461ea2
Guilherme Wang de Faria barros <wangbarros@gmail.com>
2025-11-19 15:40:09 +0100

    Fixed typo bug in filtering

--- filter_data_test.R
+++ filter_data_test.R
@@ -18,22 +18,22 @@ df2022_filtered  = df2022[df2022$CNT %in% c(c_list),]
 write.csv(df2022_filtered,'data/df2022_filtered.csv', row.names = FALSE)
 
 df2018_filtered  = df2018[df2018$CNT %in% c(c_list),]
-write.csv(df2022_filtered,'data/df2018_filtered.csv', row.names = FALSE)
+write.csv(df2018_filtered,'data/df2018_filtered.csv', row.names = FALSE)
 
 df2015_filtered  = df2015[df2015$CNT %in% c(c_list),]
-write.csv(df2022_filtered,'data/df2015_filtered.csv', row.names = FALSE)
+write.csv(df2015_filtered,'data/df2015_filtered.csv', row.names = FALSE)
 
 df2012_filtered  = df2012[df2012$CNT %in% c(c_list),]
-write.csv(df2022_filtered,'data/df2012_filtered.csv', row.names = FALSE)
+write.csv(df2012_filtered,'data/df2012_filtered.csv', row.names = FALSE)
 
 df2009_filtered  = df2009[df2009$CNT %in% c(c_list),]
-write.csv(df2022_filtered,'data/df2009_filtered.csv', row.names = FALSE)
+write.csv(df2009_filtered,'data/df2009_filtered.csv', row.names = FALSE)
 
 df2006_filtered  = df2006[df2006$CNT %in% c(c_list),]
-write.csv(df2022_filtered,'data/df2006_filtered.csv', row.names = FALSE)
+write.csv(df2006_filtered,'data/df2006_filtered.csv', row.names = FALSE)
 
 df2003_filtered  = df2003[df2003$CNT %in% c(c_list),]
-write.csv(df2022_filtered,'data/df2003_filtered.csv', row.names = FALSE)
+write.csv(df2003_filtered,'data/df2003_filtered.csv', row.names = FALSE)
 
 
 
